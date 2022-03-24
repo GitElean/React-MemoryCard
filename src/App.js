e43d0147ec6ddc0200a041c05586f305db937f61
GitElean <eleanjulian@gmail.com>
2022-03-24 15:15:01 -0600

    scratch

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+function App () {
+  return (
+        <div className='App'>
+            <h1>Memoria</h1>
+            <button>Juego nuevo</button>
+        </div>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = requiere('path')
+const HtmlWwebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    static: path.resolve(__dirname, './dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.png/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.(png|jpe?g|gif|mp4)$/i,
+        loader: 'file-loader',
+        options: {
+          outputPath: 'images',
+        }
+      }
+    ]
+  }
+}
