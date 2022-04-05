197be8a9c0a893d81ba7c5bf97267d64a3fdc6be
GitElean <eleanjulian@gmail.com>
2022-04-05 15:31:09 -0600

    cambio de build y correcciones

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
-const presets = ['@babel/preset-env', '@babel/preset-react']
-
-module.exports = { presets }
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-react',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'entry',
+      },
+    ],
+  ],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,36 @@
-const path = requiere('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production',
-  entry: {
-    main: './src/index.js'
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js'
-  },
-  devServer: {
-    static: path.resolve(__dirname, './dist')
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'index.html'
-    })
-  ],
   module: {
     rules: [
       {
-        test: /\.png/,
-        type: 'asset/resource'
-      },
-      {
-        test: /\.css$/i,
-        include: path.resolve(__dirname, 'src'),
-        use: ['style-loader', 'css-loader', 'postcss-loader']
-      },
-      {
-        test: /\.html$/i,
-        loader: 'html-loader'
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
+        test: /\.(js|jsx)$/,
+        exclude: /nodeModules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+        ],
       },
       {
         test: /\.(png|jpe?g|gif|mp4)$/i,
         loader: 'file-loader',
         options: {
-          outputPath: 'images'
-        }
-      }
-    ]
-  }
-}
+          outputPath: 'images',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+  ],
+};
